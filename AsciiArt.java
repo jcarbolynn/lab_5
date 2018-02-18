74262f792189fd44e96eb43d17545c7947c1a02c
jcarbolynn <jcarbolynn@ufl.edu>
2018-02-17 23:48:20 -0500

    first commit lab 5

---
+++ AsciiArt.java
@@ -0,0 +1,30 @@
+public class AsciiArt {
+
+   public static void main(String[] args) {
+
+
+      // Draw tree
+
+      System.out.println("   *");
+
+      System.out.println("  ***");
+
+      System.out.println(" *****");
+
+      System.out.println("*******");
+
+      System.out.println("  ***");
+
+      
+System.out.println("\n" + "\n" + "/\\   /\\");
+
+      System.out.println("  o o");
+
+      System.out.println(" =   =");
+
+      System.out.println("  ---");
+
+   //another comment
+   }
+
+}
